2c158d588e3f6bdfb8f0b04e8853b29db42a751d
Sarath <learn.cypher@gmail.com>
2025-10-15 11:04:13 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { Routes,Route } from "react-router-dom";
-import HomePage from "./pages/homePage";
+import React from 'react'
 
-const App = () =>{
-  return(
-    <div className="">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
+const App = () => {
+
+    const name = 'Hello world!!';
+    const x =10;
+    const y = 20;
+    const names = ['Brad','mary','joe', 'sara'];
+    const loggedin =true;
+    const style1 = { color:"green", fontSize:"1.5rem"};
+
+  return (
+    <>
+      <div className='text-xl'> 
+          <h1 style={style1} >{name}</h1>
+          <p>{names.toLocaleString()}</p>
+      </div>
+      <p className="text-fuchsia-800">Lorem ipsum dolor sit amet consectetur</p> 
+      <p>
+          the sum of {x} and {y} is {x + y}
+      </p>
+      <ul>
+      {names.map((name, index) => (
+          <li key={index}>{name}</li>
+      ))}
+      </ul>
+      { loggedin ? "login done" : "not logged"}
+    </>
   )
 }
 
