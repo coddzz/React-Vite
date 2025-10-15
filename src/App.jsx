fb89355eb86c90795b1b4ca8ca5d146e82d42990
Sarath <learn.cypher@gmail.com>
2025-10-15 12:47:09 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,48 @@
-import React from 'react'
+import logo from "./images/logo.svg"
 
 const App = () => {
 
-    const name = 'Hello world!!';
-    const x =10;
-    const y = 20;
-    const names = ['Brad','mary','joe', 'sara'];
-    const loggedin =true;
-    const style1 = { color:"green", fontSize:"1.5rem"};
-
   return (
     <>
-      <div className='text-xl'> 
-          <h1 style={style1} >{name}</h1>
-          <p>{names.toLocaleString()}</p>
-      </div>
-      <p className="text-fuchsia-800">Lorem ipsum dolor sit amet consectetur</p> 
-      <p>
-          the sum of {x} and {y} is {x + y}
-      </p>
-      <ul>
-      {names.map((name, index) => (
-          <li key={index}>{name}</li>
-      ))}
-      </ul>
-      { loggedin ? "login done" : "not logged"}
+      <nav className="bg-indigo-700 border-b border-black">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="flex h-20 items-center justify-between">
+            <div
+              className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+            >
+              <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+                <img
+                  className="h-10 w-auto"
+                  src={logo}
+                  alt="React Jobs"
+                />
+                <span className="hidden md:block text-white text-2xl font-bold ml-2">
+                  React Jobs
+                </span>
+              </a>
+              <div className="md:ml-auto">
+                <div className="flex space-x-2">
+                  <a
+                    href="/index.html"
+                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    >Home</a>
+                  <a
+                    href="/jobs.html"
+                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    >Jobs</a>
+                  <a
+                    href="/add-job.html"
+                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    >Add Job</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
 
-export default App;
+export default App
+
