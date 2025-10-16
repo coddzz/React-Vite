33c2e646dd9a07edc435df7e3b7e7bf0fbcabef0
Sarath <learn.cypher@gmail.com>
2025-10-16 21:06:45 +0530

    JobListing. Added JSON

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,33 +1,16 @@
 import React from 'react'
+import {jobs} from '../jobs.json'
+import JobListing from './JobListing'
 
 const JobListings = () => {
+
   return (
     <>
-    <div className="bg-white rounded-2xl border-2 border-indigo-300 m-2 shadow-md relative">
-        <div className="p-4">
-            <div className="mb-6">
-                <div className="text-gray-600 my-2">Remote</div>
-                <h3 className="text-xl font-bold">Front-End Engineer</h3>
-
-                <div className="mb-5">
-                    Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
-                </div>
-
-                <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
-
-                <div className="border border-gray-300 mb-5"></div>
-
-                <div className="flex flex-col lg:flex-row justify-between mb-4">
-                    <div className="text-blue-950 mb-3">
-                        Miami, FL
-                    </div>
-                <a href="" 
-                className="h-[36px] bg-indigo-700 hover:bg-indigo-800 text-white px-4 py-2 rounded-lg text-center text-sm">
-                    Read More
-                </a>
-                </div>
-            </div>
-        </div>
+    <h2 className='mx-15 my-5 px-4 py-2 bg-sky-200 text-gray-700 font-extrabold text-xl shadow-md'>Start Your Career Today.. &#128395; </h2>
+    <div className='mx-10 grid grid-cols-1 md:grid-cols-3 gap-6'>
+        {jobs.map((job) => (
+        <JobListing key={job.id} job={job}/>
+        ))}
     </div>
     </>
   )
