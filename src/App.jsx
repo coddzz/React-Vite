c07d90f1bf8c49a6dca5b4ba4e4497f3be60d945
Sarath <learn.cypher@gmail.com>
2025-10-17 15:08:38 +0530

    Nested Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage'
+import MainLayout from './layouts/MainLayout'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Homepage/>}></Route>
+        <Route path='/' element={<MainLayout/>}>
+          <Route index element={<Homepage/>}/>
+          {/*Nested Routes*/}
+        </Route>
       </Routes>
     </BrowserRouter>
   )
