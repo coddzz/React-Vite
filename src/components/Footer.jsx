c5b864a4b2a65233ad5dd70a9e14cf40c8b252e2
Sarath <learn.cypher@gmail.com>
2025-10-18 13:02:41 +0530

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+
+    const year = new Date();
+  return (
+    <div>
+      <div className='relative bottom-0 left-0 w-full py-5 bg-indigo-700 text-center border-t-4 border-indigo-400 text-gray-100 font-sans'>Copyright &copy; {year.getFullYear()} </div>
+    </div>
+  )
+}
+
+export default Footer
