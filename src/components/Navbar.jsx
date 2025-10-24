c1dec0362a2547a99ca02695d5031d122db5e9a5
Sarath <learn.cypher@gmail.com>
2025-10-24 19:20:29 +0530

    framer-motion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import logo from "../images/logo.svg"
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
 
@@ -12,14 +13,20 @@ const Navbar = () => {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-              <NavLink className="flex flex-shrink-0 items-center mr-1" to="/">
-                <img 
-                className="md:h-10 h-7 w-auto"
-                src={logo} 
-                alt="reacttest" />
-              </NavLink>
-              <span className="hidden md:block text-white text-2xl font-bold ml-2">React</span>
-              <span className="md:hidden text-white text-2xl font-bold ">R</span>
+              <motion.div
+                  initial={{ opacity: 0, x: -50 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.5 }}
+                  className='flex flex-row items-center justify-center'>
+                <NavLink className="flex flex-shrink-0 items-center mr-1" to="/">
+                  <img 
+                  className="md:h-10 h-7 w-auto"
+                  src={logo} 
+                  alt="reacttest" />
+                </NavLink>
+                <span className="hidden md:block text-white text-2xl font-bold ml-2">React</span>
+                <span className="md:hidden text-white text-2xl font-bold ">R</span>
+              </motion.div>
               <div className="md:ml-auto">
                 <div className="flex space-x-6 ml-10">
                   <NavLink to="/"
